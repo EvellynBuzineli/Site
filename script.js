507d94938cd66d16c1f12287c2ba05c8d05e5050
EvellynBuzineli <168847991+EvellynBuzineli@users.noreply.github.com>
2025-09-26 14:14:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const body = document.body;
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+      // Linha removida para impedir o chacoalhar
+      // botaoDeAcessibilidade.classList.toggle('rotacao-botao'); 
+      opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+      const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+      botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+    
+    })
+
+      const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+      const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+      const alternaContraste = document.getElementById('alterna-contraste')
+
+      let tamanhoAtualFonte = 1;
+
+      aumentaFonteBotao.addEventListener('click', function(){
+          tamanhoAtualFonte += 0.1;
+          body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+      })
+
+      diminuiFonteBotao.addEventListener('click', function(){
+          tamanhoAtualFonte -= 0.1;
+          body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+      })
+
+      alternaContraste.addEventListener('click', function(){
+          body.classList.toggle('alto-contraste')
+      })
+
+
+})
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#the-mamas-papas-section', { delay: 500 }); 
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
